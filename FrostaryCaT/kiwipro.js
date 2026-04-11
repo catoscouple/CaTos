90057856341ad87585590ae5e2373ea29151abfb
catoscouple <catoscouple@gmail.com>
2026-04-11 14:58:27 -0600

    wewe

---
+++ FrostaryCaT/kiwipro.js
@@ -0,0 +1,7 @@
+//peme//
+const owo = document.getElementById("bear")
+//script know bear now//
+owo.addEventListener("click", () => {
+    owo.style.animation = "owokiwiproOut 3s ease forwards"
+    owo.style.pointerEvents = "none"
+})
